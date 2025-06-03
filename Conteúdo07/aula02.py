086ab97d80bf411065a81cef9d05f04450c4568d
Alisson Lira <3deelira@gmail.com>
2025-06-02 21:24:12 -0300

    teste

---
+++ Conteúdo07/aula02.py
@@ -0,0 +1,18 @@
+'''
+modo de arquivo:
+w - cria o arquivo, sobrescrevendo e esreve nele
+a - adiciona no final do arquivo
+
+'''
+
+new_arquivo = open('listaalunos.txt', 'w')
+
+new_arquivo.write('olá, meu nome é Lira \n')
+
+new_arquivo.close()
+
+'''
+
+import os = para poder criar pastas
+
+'''
